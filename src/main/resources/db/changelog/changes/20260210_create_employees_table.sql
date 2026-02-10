c0852f9e70208835990cfbe9b9cc93608949e168
sajinb <sajin.balakrishnan@gmail.com>
2026-02-10 15:10:48 +0530

    feat(sql): create employees table to match entity

---
+++ src/main/resources/db/changelog/changes/20260210_create_employees_table.sql
@@ -0,0 +1,11 @@
+-- SQL DDL to create employees table
+CREATE TABLE IF NOT EXISTS employees (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(800) NOT NULL,
+  last_name VARCHAR(150) NOT NULL,
+  project_name VARCHAR(500),
+  email VARCHAR(255) NOT NULL,
+  position VARCHAR(200),
+  salary DECIMAL(15,2),
+  hired_date DATE
+);
